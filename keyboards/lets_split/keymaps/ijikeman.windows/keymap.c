ebd7c1bb86a657f8d389f2d4b3c614a19c7032b5
ijikeman <keiji.ue@gmail.com>
2019-10-03 13:09:11 +0900

    replace enter to quote

--- keyboards/lets_split/keymaps/ijikeman.windows/keymap.c
+++ keyboards/lets_split/keymaps/ijikeman.windows/keymap.c
@@ -51,17 +51,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * ,-----------------------------------------------------------------------------------.
 * |      |  Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  [/] |
 * |------+------+------+------+------+-------------+------+------+------+------+------|
-* | CTR  |  A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+* | CTR  |  A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | '/\ |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
-* |ESC/SFT|  Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  /  | '/\  |
+* |ESC/SFT|  Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  /  |ENT/SFT|
 * |------+------+------+------+------+------+------+------+------+------+------+------|
 * |      |      |      |  ALT | GUI  |LOW/SPC|RAI/BS| IME | LED  |      l     |      |
 * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_ortho_4x12(
    KC_ESC,       KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC, \
-   CTL_T(KC_TAB), KC_A,KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-   SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, M_QUOT, \
+   CTL_T(KC_TAB), KC_A,KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, M_QUOT, \
+   SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT), \
    _______,  _______,  _______, KC_LALT, KC_LGUI,LT(_LOWER,KC_SPC),LT(_RAISE,KC_BSPC), M_LANG, LED,_______,_______,_______  \
 ),
 
