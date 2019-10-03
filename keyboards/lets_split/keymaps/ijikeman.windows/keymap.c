3e2a72151b1b61b03f60f2d785c9306489a3e28e
ijikeman <keiji.ue@gmail.com>
2019-10-03 12:55:49 +0900

    replace new macros

--- keyboards/lets_split/keymaps/ijikeman.windows/keymap.c
+++ keyboards/lets_split/keymaps/ijikeman.windows/keymap.c
@@ -9,10 +9,9 @@ extern keymap_config_t keymap_config;
 #define _BASE 0
 #define _LOWER 1
 #define _RAISE 2
-#define _MOUSE 3
+//#define _MOUSE 3
 #define _LED 4
 
-#define MACRO_TMUX_LANG 31
 #define MACRO_TMUX_BRC 32
 #define MACRO_TMUX_MINUS 33
 #define MACRO_TMUX_S_MINUS 34
@@ -22,15 +21,11 @@ extern keymap_config_t keymap_config;
 #define MACRO_COPY 41
 #define MACRO_PASTE 42
 
-#define M_LANG M(MACRO_TMUX_LANG)
-#define M_BRC   M(MACRO_TMUX_BRC)
-#define M_MINUS   M(MACRO_TMUX_MINUS)
-#define M_S_MINUS   M(MACRO_TMUX_S_MINUS)
-#define M_QUOT   M(MACRO_TMUX_QUOT)
-
+/*
 #define M_CUT   M(MACRO_CUT)
 #define M_COPY   M(MACRO_COPY)
 #define M_PASTE   M(MACRO_PASTE)
+*/
 
 #define PUSH_TIME 75
 
@@ -38,8 +33,13 @@ enum custom_keycodes {
   BASE = SAFE_RANGE,
   LOWER,
   RAISE,
-  MOUSE,
+//  MOUSE,
   LED,
+  M_LANG,
+  M_BRC,
+  M_MINUS,
+  M_S_MINUS,
+  M_QUOT
 };
 
 // Fillers to make layering more clear
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  _/+ |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      | LEFT | DOWN |  UP  | RGHT |      |      |
+ * | CAPS |      |      |      |      |      | LEFT | DOWN |  UP  | RGHT |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  -/= |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      | LEFT | DOWN |  UP  | RGHT |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | F7   |  F8  |  F9  | F10  | F11  |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -97,6 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   M_MINUS, \
+  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,   _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET \
@@ -112,13 +113,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
- */
 [_MOUSE] =  LAYOUT_ortho_4x12( \
   _______, _______, _______, KC_MS_U, _______, KC_WREF, KC_F7,  KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, \
   _______, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, KC_WBAK, KC_LEFT,KC_DOWN, KC_UP, KC_RGHT,   _______, _______, \
   _______, KC_BTN2, KC_WH_D, _______, KC_WH_U, KC_WFWD, M_CUT,  M_COPY,  M_PASTE, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______ \
 ),
+ */
 
 [_LED] =  LAYOUT_ortho_4x12( \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
@@ -134,6 +135,7 @@ void persistent_default_layer_set(uint16_t default_layer) {
   default_layer_set(default_layer);
 }
 
+static uint16_t key_timer;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case BASE:
@@ -158,7 +160,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case MOUSE:
+/*
+ * case MOUSE:
       if (record->event.pressed) {
         layer_on(_MOUSE);
       } else {
@@ -166,6 +169,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+*/
     case LED:
       if (record->event.pressed) {
         layer_on(_LED);
@@ -174,65 +178,63 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    // Original Macro
+    case M_LANG:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTRL(SS_TAP(X_SPACE)));
+      }
+      break;
+    case M_BRC:
+        if (record->event.pressed) {
+          key_timer = timer_read();
+        } else {
+          if (timer_elapsed(key_timer) >= PUSH_TIME) {
+            SEND_STRING(SS_TAP(X_RBRACKET));
+          } else {
+            SEND_STRING(SS_TAP(X_LBRACKET));
+          }
+        }
+        break;
+    case M_MINUS:
+        if (record->event.pressed) {
+          key_timer = timer_read();
+        } else {
+          if (timer_elapsed(key_timer) >= PUSH_TIME) {
+            SEND_STRING(SS_TAP(X_EQUAL));
+          } else {
+            SEND_STRING(SS_TAP(X_MINUS));
+          }
+        }
+        break;
+    case M_S_MINUS:
+        if (record->event.pressed) {
+          key_timer = timer_read();
+        } else {
+          if (timer_elapsed(key_timer) >= PUSH_TIME) {
+            SEND_STRING(SS_LSFT(SS_TAP(X_EQUAL)));
+          } else {
+            SEND_STRING(SS_LSFT(SS_TAP(X_MINUS)));
+          }
+        }
+        break;
+    case M_QUOT:
+        if (record->event.pressed) {
+          key_timer = timer_read();
+        } else {
+          if (timer_elapsed(key_timer) >= PUSH_TIME) {
+            SEND_STRING(SS_TAP(X_BSLASH));
+          } else {
+            SEND_STRING(SS_TAP(X_QUOTE));
+          }
+        }
+        break;
   }
   return true;
 }
-
-static uint16_t key_timer;
+/*
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
       switch(id) {
-        case MACRO_TMUX_LANG:
-        if (record->event.pressed) {
-          key_timer = timer_read();
-        } else {
-          return MACRO(D(LCTL), T(SPC), U(LCTL), END);
-        }
-        break;
-        case MACRO_TMUX_BRC:
-        if (record->event.pressed) {
-          key_timer = timer_read();
-        } else {
-          if (timer_elapsed(key_timer) >= PUSH_TIME) {
-            return MACRO(T(RBRC), END);
-          } else {
-            return MACRO(T(LBRC), END);
-          }
-        }
-        break;
-        case MACRO_TMUX_MINUS:
-        if (record->event.pressed) {
-          key_timer = timer_read();
-        } else {
-          if (timer_elapsed(key_timer) >= PUSH_TIME) {
-            return MACRO(T(EQL), END);
-          } else {
-            return MACRO(T(MINUS), END);
-          }
-        }
-        break;
-        case MACRO_TMUX_S_MINUS:
-        if (record->event.pressed) {
-          key_timer = timer_read();
-        } else {
-          if (timer_elapsed(key_timer) >= PUSH_TIME) {
-            return MACRO(D(LSFT), T(EQL), U(LSFT), END);
-          } else {
-            return MACRO(D(LSFT), T(MINUS), U(LSFT), END);
-          }
-        }
-        break;
-        case MACRO_TMUX_QUOT:
-        if (record->event.pressed) {
-          key_timer = timer_read();
-        } else {
-          if (timer_elapsed(key_timer) >= PUSH_TIME) {
-            return MACRO(T(BSLS), END);
-          } else {
-            return MACRO(T(QUOT), END);
-          }
-        }
-        break;
         case MACRO_CUT:
         if (record->event.pressed) {
           return MACRO(D(LCTL), T(X), U(LCTL), END);
@@ -251,3 +253,4 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       }
       return MACRO_NONE;
 };
+*/
