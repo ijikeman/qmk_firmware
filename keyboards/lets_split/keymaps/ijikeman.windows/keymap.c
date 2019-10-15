61a619fdd91bea5bb718b5487d5e48517851dd46
ijikeman <keiji.ue@gmail.com>
2019-10-16 08:51:32 +0900

    fixed keymap

--- keyboards/lets_split/keymaps/ijikeman.windows/keymap.c
+++ keyboards/lets_split/keymaps/ijikeman.windows/keymap.c
@@ -61,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_ortho_4x12(
    KC_ESC,       KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC, \
    CTL_T(KC_TAB), KC_A,KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, M_QUOT, \
-   SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT), \
-   _______,  _______,  _______, KC_LALT, KC_LGUI,LT(_LOWER,KC_SPC),LT(_RAISE,KC_BSPC), LED,_______,_______,_______,_______  \
+   SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
+   _______,  _______,  _______, KC_LALT, KC_LGUI,LT(_LOWER,KC_SPC),LT(_RAISE,KC_BSPC), M_LANG, LED,_______,_______,_______  \
 ),
 
 
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
   S(KC_GRV),S(KC_1),S(KC_2), S(KC_3), S(KC_4),S(KC_5),S(KC_6), S(KC_7), S(KC_8), S(KC_9),S(KC_0), M_S_MINUS, \
-  KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,   _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,   _______, _______, \
+  KC_CAPS, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
   RESET, _______, _______, _______, _______, _______, KC_DEL, _______, _______, _______, _______, _______ \
 ),
 
